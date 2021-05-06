d2ca0d401b6bcd132c0faa692dafc4cc912f17c8
Rodrigo Ida <rodrigoida1@hotmail.com>
2021-05-06 10:57:40 -0300

    insere funcao autocomplete para campo de pesquisa no header

--- header-form-handler.js
+++ header-form-handler.js
@@ -1,40 +1,166 @@
-{/* <button class="show-header-form"><img src="svg/header/search.svg"/></button>
+function showform() {
+  let showButton = document.querySelector(".show-header-form-btn");
+  let formDiv = document.querySelector(".input-wrapper");
+  let closeBtn = document.querySelector(".hide-input-button");
 
-<div class="input-wrapper">
+  showButton.addEventListener("click", () => {
+    formDiv.classList.add("show-search-form");
+  });
+  closeBtn.addEventListener("click", () => {
+    formDiv.classList.remove("show-search-form");
+  });
+}
 
-  <button class="hide-input-button">&lt;</button> */}
+showform();
+
+const formHandler = () => {
+  const form = document.querySelector(".header-form");
+  form.addEventListener("submit", (event) => {
+    event.preventDefault();
+
+    const searchInputValue = document.querySelector("#search").value;
+
+    form.action = `https://www.remindbrindes.com.br/catalogo.php?search=${searchInputValue}`;
+    form.submit();
+  });
+};
+
+formHandler();
 
 
-  function showform(){
-      
-    //   let formDiv = document.querySelector('.')
-
-
-    let showButton = document.querySelector('.show-header-form-btn')
-    let formDiv = document.querySelector('.input-wrapper')
-    let closeBtn = document.querySelector('.hide-input-button')
-
-    showButton.addEventListener('click', ()=>{
-        formDiv.classList.add('show-search-form')
-    })
-    closeBtn.addEventListener('click', ()=>{
-        formDiv.classList.remove('show-search-form')
-    })
-
+const autocomplete = (inp, arr) => {
+  /*the autocomplete function takes two arguments,
+      the text field element and an array of possible autocompleted values:*/
+  let currentFocus;
+  /*execute a function when someone writes in the text field:*/
+  inp.addEventListener("input", function (e) {
+    let a,
+      b,
+      i,
+      val = this.value;
+    /*close any already open lists of autocompleted values*/
+    closeAllLists();
+    if (!val) {
+      return false;
+    }
+    currentFocus = -1;
+    /*create a DIV element that will contain the items (values):*/
+    a = document.createElement("DIV");
+    a.setAttribute("id", this.id + "autocomplete-list");
+    a.setAttribute("class", "autocomplete-items");
+    /*append the DIV element as a child of the autocomplete container:*/
+    this.parentNode.appendChild(a);
+    /*for each item in the array...*/
+    for (i = 0; i < arr.length; i++) {
+      /*check if the item starts with the same letters as the text field value:*/
+      if (arr[i].substr(0, val.length).toUpperCase() == val.toUpperCase()) {
+        /*create a DIV element for each matching element:*/
+        b = document.createElement("DIV");
+        /*make the matching letters bold:*/
+        b.innerHTML = "<strong>" + arr[i].substr(0, val.length) + "</strong>";
+        b.innerHTML += arr[i].substr(val.length);
+        /*insert a input field that will hold the current array item's value:*/
+        b.innerHTML += "<input type='hidden' value='" + arr[i] + "'>";
+        /*execute a function when someone clicks on the item value (DIV element):*/
+        b.addEventListener("click", function (e) {
+          /*insert the value for the autocomplete text field:*/
+          inp.value = this.getElementsByTagName("input")[0].value;
+          /*close the list of autocompleted values,
+                  (or any other open lists of autocompleted values:*/
+          closeAllLists();
+        });
+        a.appendChild(b);
+      }
+    }
+  });
+  /*execute a function presses a key on the keyboard:*/
+  inp.addEventListener("keydown", function (e) {
+    let x = document.getElementById(this.id + "autocomplete-list");
+    if (x) x = x.getElementsByTagName("div");
+    if (e.keyCode == 40) {
+      /*If the arrow DOWN key is pressed,
+            increase the currentFocus variable:*/
+      currentFocus++;
+      /*and and make the current item more visible:*/
+      addActive(x);
+    } else if (e.keyCode == 38) {
+      //up
+      /*If the arrow UP key is pressed,
+            decrease the currentFocus variable:*/
+      currentFocus--;
+      /*and and make the current item more visible:*/
+      addActive(x);
+    } else if (e.keyCode == 13) {
+      /*If the ENTER key is pressed, prevent the form from being submitted,*/
+      e.preventDefault();
+      if (currentFocus > -1) {
+        /*and simulate a click on the "active" item:*/
+        if (x) x[currentFocus].click();
+      }
+    }
+  });
+  function addActive(x) {
+    /*a function to classify an item as "active":*/
+    if (!x) return false;
+    /*start by removing the "active" class on all items:*/
+    removeActive(x);
+    if (currentFocus >= x.length) currentFocus = 0;
+    if (currentFocus < 0) currentFocus = x.length - 1;
+    /*add class "autocomplete-active":*/
+    x[currentFocus].classList.add("autocomplete-active");
   }
-
-  showform();
-
-  const formHandler = () => {
-    const form = document.querySelector('.header-form')
-    form.addEventListener('submit', event => {
-      event.preventDefault();
-
-      const searchInputValue = document.querySelector('#search').value
-
-      form.action = `https://www.remindbrindes.com.br/catalogo.php?search=${searchInputValue}`
-      form.submit()
-    })
+  function removeActive(x) {
+    /*a function to remove the "active" class from all autocomplete items:*/
+    for (let i = 0; i < x.length; i++) {
+      x[i].classList.remove("autocomplete-active");
+    }
   }
+  function closeAllLists(elmnt) {
+    /*close all autocomplete lists in the document,
+        except the one passed as an argument:*/
+    let x = document.getElementsByClassName("autocomplete-items");
+    for (let i = 0; i < x.length; i++) {
+      if (elmnt != x[i] && elmnt != inp) {
+        x[i].parentNode.removeChild(x[i]);
+      }
+    }
+  }
+  /*execute a function when someone clicks in the document:*/
+  document.addEventListener("click", function (e) {
+    closeAllLists(e.target);
+  });
+};
 
-  formHandler()
+
+let sugestions = ["agendas", "almofadas", "apoio para copo", "aromatizador",
+"brindes de madeira", "brindes ecologicos", "brindes sofisticados", "blocos personalizados", "calendarios", "cadernetas", "chaveiros", 
+"cuidados pessoais", "calculadoras",
+ "canetas", "champagne", 
+ "eventos e congresso",
+ "ferramentas",
+ "fitness",
+ , "jogos", "kits",
+"kits de culivo",
+"kit spa",
+"kit churrasco",
+"kit queijo",
+"kit vinho",
+"kit tempero",
+"kits especiais",
+"kit whisky",
+"memo boards",
+"mandalas",
+"massageadores",
+"marcadores de pagina",
+"porta objetos",
+"porta chave",
+"porta retrato",
+"relogios",
+"suporte para pc",
+"suporte para celular",
+"squeezes",
+"tecnologia - power bank",];
+
+let input = document.querySelector('#search')
+
+autocomplete(input, sugestions);
